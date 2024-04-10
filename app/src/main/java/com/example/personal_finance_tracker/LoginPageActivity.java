8ca19026a20e5a8232703250753e08900e10bb5f
Nicholai Martinsen <nicfox77@gmail.com>
2024-04-09 23:25:04 -0700

    Working on adding LoginPageActivity

--- app/src/main/java/com/example/personal_finance_tracker/LoginPageActivity.java
+++ app/src/main/java/com/example/personal_finance_tracker/LoginPageActivity.java
@@ -2,21 +2,24 @@ package com.example.personal_finance_tracker;
 
 import android.os.Bundle;
 
-import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
-import androidx.core.graphics.Insets;
-import androidx.core.view.ViewCompat;
-import androidx.core.view.WindowInsetsCompat;
 
-public class MainActivity extends AppCompatActivity {
+import java.util.concurrent.CountedCompleter;
+
+
+
+public class LoginPageActivity extends AppCompatActivity {
+    ActivityLoginPageactivity binding;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-
-
+        binding = ActivityMainBinding.inflate(getLayoutInflater());
+        View view = binding.getRoot();
 
         setContentView(R.layout.activity_main);
 
     }
+
+
 }
